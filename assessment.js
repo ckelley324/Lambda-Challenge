20673bfbbb663aadbd52d06bcab1ec1cd5520acc
Crystal Kelley <ckelley324@gmail.com>
2018-03-01 17:44:27 -0500

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+  return "Hello World!";
 }
 
 /*
@@ -23,8 +23,16 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    return "Lambda School";
+  } else if (num % 3 == 0) {
+    return "Lambda";
+  } else if (num % 5 == 0) {
+    return "School";
+  } else {
+    return num;
+  }
 }
 
 /*
@@ -38,8 +46,22 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
-
+function longestString(strs) {
+  let longValue = 0
+  let longestWord = ""
+  for (i = 0; i < strs.length; i++) {
+    if (strs[i].length > longValue) {
+      longValue = strs[i].length;
+      longestWord = strs[i];
+    }
+  }
+  for (i = 0; i < strs.length; i++) {
+    if (strs[i].length == longValue) {
+    longValue = longValue
+    longestWord = longestWord;
+  }
+}
+    return longestWord;
 }
 
 /*
@@ -63,8 +85,13 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
-
+function computeUserAverageAge(users) {
+  let ages = [];
+  for (i = 0; i < users.length; i++) {
+    ages.push(users[i].age);
+  }
+  let avg = ages.reduce((x,y) => x + y, 0) / ages.length
+  return Math.round(avg);
 }
 
 module.exports = {
